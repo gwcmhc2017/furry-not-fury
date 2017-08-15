73ee29c71e8692c79955899e6fb7d5ecbd959895
MHC User <mhcuser@n21-30.mtholyoke.edu>
2017-08-15 15:40:29 -0400

    quiz questions working; still working on answers

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
-$(document).ready(function() {
+$(document).ready(function(){
   //example code - feel free to delete!
-  var timesSillyButtonClicked = 0;
-  $('#silly-button').click(function() {
-    timesSillyButtonClicked++;
-    $('#silly-button-text').text(
-      'You clicked the silly button ' + timesSillyButtonClicked + ' times!',
-    );
-  });
+  // var timesSillyButtonClicked = 0;
+  $('#yes1').click(function() {
+   $('#declawingAnswer2').show();
+   $('#declawing').hide();
 
-  //write your code here!
+
 });
+$('#no1').click(function() {
+ $('#declawingAnswer1').show();
+ $('#declawing').hide();
+
+
+});
+$('#yes2').click(function() {
+ $('#farmingAnswer1').show();
+ $('#farming').hide();
+
+
+});
+$('#no2').click(function() {
+ $('#farmingAnswer2').show();
+ $('#farming').hide();
+
+
+});
+$('#yes3').click(function() {
+ $('#testingAnswer1').show();
+ $('#testing').hide();
+
+
+});
+$('#no3').click(function() {
+ $('#testingAnswer2').show();
+ $('#testing').hide();
+
+
+});
+
+});
+// +      // console.log("poop");
+// +      $("#").show();
+// +    } else if (timesAButtonClicked < timesBButtonClicked) {
+// +      $("#Olaf").show();
+
+//     timesSillyButtonClicked++;
+//     $('#silly-button-text').text(
+//       'You clicked the silly button ' + timesSillyButtonClicked + ' times!',
+//     );
+//   });
+//
+//   //write your code here!
+// });
