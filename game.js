9d5018e052f6d121e11dd10c05a3a99b79b1d14a
MHC User <mhcuser@n21-226.mtholyoke.edu>
2017-08-17 15:41:42 -0400

    dun with game

--- game.js
+++ game.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
   var distanceFromLeft = 0;
     var distanceFromTop = 0;
+    var canIPlay = true
+
   $(document).keydown(function(event){
+    if(canIPlay){
     if (event.keyCode === 39){
       distanceFromLeft +=25;
       $('#cat').css('left', distanceFromLeft);
@@ -14,32 +17,46 @@ $(document).ready(function(){
 
     if (event.keyCode === 38){
       distanceFromTop -=25;
-      $('#cat').css('top', distanceFromTop);
+    $('#cat').css('top', distanceFromTop);
     }
     if (event.keyCode === 40){
       distanceFromTop +=25;
       $('#cat').css('top', distanceFromTop);
     }
+  }
     if (distanceFromTop === 775 && (distanceFromLeft===1500|| distanceFromLeft===850|| distanceFromLeft===50)){
-
+canIPlay = false
       // $('#help1').show();
     var random =Math.random();
     if (random<0.25){
+
       $("#help1").show();
       $(".button").show();
     }else if (random < 0.5 && random > 0.25){
         $("#help2").show();
         $(".button").show();
+
     }else if(random<0.75 && random > 0.5){
         $("#help3").show();
         $(".button").show();
+
     }else if (random < 1.0 && random > 0.75){
       $("#help4").show();
         $(".button").show();
-    }
 
-
-
-    }
-  })
+    };
+   $('.button').click(function(){
+    canIPlay = true;
+   $('#cat').css('left',0);
+  $('#cat').css('top',0);
+  $("#help1").hide();
+  $("#help2").hide();
+  $("#help3").hide();
+  $("#help4").hide()
 })
+}
+})
+})
+
+
+// })
